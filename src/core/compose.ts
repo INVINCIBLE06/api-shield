daff94d42c4670f9f08bfe75907470f38863d193
Saurabh <sp832154@gmail.com>
2026-07-04 23:47:23 +0530

    Middleware started

--- src/core/compose.ts
+++ src/core/compose.ts
@@ -1,7 +1,3 @@
-import { NextFunction, Request, Response } from "express";
+export function compose(middlewares: []) {
 
-export function compose(req: Request, res: Response, next: NextFunction) {
-
-
-    next();
 }
--- src/core/shield.ts
+++ src/core/shield.ts
@@ -1,12 +1,23 @@
-import type { RequestHandler } from "express";
+
 import { ShieldOptions } from "./types";
 import { normalize } from "../config";
 
-export function shield(options: ShieldOptions): RequestHandler {
+export function shield(options: ShieldOptions) {
     const config = normalize(options);
-    return (req, res, next) => {
-        // TODO:
-        // Load Middlewares
-        next();
-    };
-}
+    // const middlewares = [];
+    if (config.rateLimiter && config.rateLimiter.enabled) {
+        // middlewares.push(rateLimiter(config.rateLimiter));
+    }
+    if (config.ipBlocker && config.ipBlocker.enabled) {
+        // middlewares.push(ipBlocker(config.ipBlocker));
+
+    }
+    if (config.requestLogger && config.requestLogger.enabled) {
+        // middlewares.push(requestLogger(config.requestLogger));
+
+    }
+    if (config.securityHeaders && config.securityHeaders.enabled) {
+        // middlewares.push(securityHeaders(config.securityHeaders));
+    }
+
+};
